3f32ff7dcd880598c5b8a25e89798d0657230f1d
Milos Stojanovic <milos92s@yahoo.com>
2015-06-27 20:05:57 +0200

    Finished initial set.

--- src/snake_pit/core.clj
+++ src/snake_pit/core.clj
@@ -3,7 +3,9 @@
   (:use fungp.util)
   (:use clojure.pprint))
 
+;;;
 ;;; Constants used in the game
+;;;
 (def MAX_STEPS "Maximum number of steps that the snake takes" 300)
 (def WIDTH "Width of the game board" 19)
 (def HEIGHT "Height of the game board" 10)
@@ -16,14 +18,17 @@
 (def RIGHT-TURN "Right turn vector" [1 -1])
 (def LEFT-TURN "Left turn vector" [-1 1])
 
+;;;
 ;;; State variables
+;;;
 (def ^:dynamic snake)
 (def ^:dynamic apple)
 (def ^:dynamic direction)
 (def ^:dynamic steps)
 
+;;;
 ;;; Util functions
-
+;;;
 (defn add-points [& pts]
   "Add vector points."
   (vec (apply map + pts)))
@@ -31,6 +36,17 @@
 (defn eats? [{[snake-head] :body} {apple :location}]
   "Check if the snake eats an apple."
   (= snake-head apple))
+
+(defn out-of-bounds? [{[head] :body}]
+  "Check if the snake is out of bounds (wall hit)"
+  (or (< (head 0) 0)
+      (> (head 0) WIDTH)
+      (< (head 1) 0)
+      (> (head 1) HEIGHT)))
+
+(defn head-overlaps-body? [{[head & body] :body}]
+  "Check if the snake has collided with itself"
+  (contains? (set body) head))
 
 (defn create-apple []
   "Create an apple."
@@ -61,8 +77,30 @@
      (= dir LEFT) (if (and (= y-head y-apple) (> x-head x-apple)) true false)
      (= dir RIGHT) (if (and (= y-head y-apple) (< x-head x-apple)) true false))))
 
-;;; Terminals
+(defn danger-ahead? [{[head] :body :as snake} dir]
+  "Check if position in front of current snake's direction is occupied by a wall or snake segment."
+  (let [next-pos (add-points head dir)]
+    (if (or (out-of-bounds? {:body (list next-pos)}) (head-overlaps-body? {:body (conj (:body snake) next-pos)}))
+      true
+      false)))
 
+(defn danger-right? [{[head] :body :as snake} dir]
+  "Check if position to the right of current snake's direction is occupied by a wall or snake segment."
+  (let [next-pos (add-points head (change-direction dir RIGHT-TURN))]
+    (if (or (out-of-bounds? {:body (list next-pos)}) (head-overlaps-body? {:body (conj (:body snake) next-pos)}))
+      true
+      false)))
+
+(defn danger-left? [{[head] :body :as snake} dir]
+  "Check if position to the left of current snake's direction is occupied by a wall or snake segment."
+  (let [next-pos (add-points head (change-direction dir LEFT-TURN))]
+    (if (or (out-of-bounds? {:body (list next-pos)}) (head-overlaps-body? {:body (conj (:body snake) next-pos)}))
+      true
+      false)))
+
+;;;
+;;; GP terminals
+;;;
 (defn turn-right []
   "Make the snake turn right."
   (set! direction (change-direction direction RIGHT-TURN))
@@ -80,13 +118,42 @@
   (set! snake (move snake direction apple))
   (set! steps (inc steps)))
 
+;;;
 ;;; GP functions
-
-(defn if-food-ahead [food no-food]
-  "Check if an apple is in line with snake's current direction."
-  `(if (!!!)
+;;;
+;;; (initial function set)
+;;;
+(defmacro if-food-ahead [food no-food]
+  "GP food ahead macro."
+  `(if (food-ahead? snake direction apple)
      ~food
      ~no-food))
+
+(defmacro if-danger-ahead [danger no-danger]
+  "GP danger ahead macro."
+  `(if (danger-ahead? snake direction)
+     ~danger
+     ~no-danger))
+
+(defmacro if-danger-right [danger-right no-danger-right]
+  "GP danger right macro."
+  `(if (danger-right? snake direction)
+     ~danger-right
+     ~no-danger-right))
+
+(defmacro if-danger-left [danger-left no-danger-left]
+  "GP danger left macro."
+  `(if (danger-left? snake direction)
+     ~danger-left
+     ~no-danger-left))
+
+;;; function (do exprs*) is used as progn2
+
+;;;
+;;; GP functions
+;;;
+;;; (full function set)
+;;;
 
 (defn -main
   "I don't do a whole lot ... yet."
--- test/snake_pit/core_test.clj
+++ test/snake_pit/core_test.clj
@@ -8,12 +8,40 @@
     (is (not= [10 11] (add-points [1 2] [3 4] [5 6])))))
 
 (deftest test-eats
-  (testing "Testing if the snake reached an apple"
+  (testing "Testing if the snake reached an apple."
   (let [snake {:body '([1 0] [1 1] [1 2])}
         apple-1 {:location [1 0]}
         apple-2 {:location [1 1]}]
     (is (= true (eats? snake apple-1)))
     (is (= false (eats? snake apple-2))))))
+
+(deftest test-out-of-bounds
+  (testing "Testing if the snake is out of bounds."
+    (let [snake-out-l {:body '([-1 5] [0 5] [1 5])}
+          snake-out-r {:body '([20 5] [19 5] [18 5])}
+          snake-out-u {:body '([10 -1] [10 0] [10 1])}
+          snake-out-d {:body '([10 11] [10 10] [10 9])}
+          snake-in-1 {:body '([0 0])}
+          snake-in-2 {:body '([19 0])}
+          snake-in-3 {:body '([0 10])}
+          snake-in-4 {:body '([19 10])}]
+      (are [snake target] (= target (out-of-bounds? snake))
+           snake-out-l true
+           snake-out-r true
+           snake-out-u true
+           snake-out-d true
+           snake-in-1 false
+           snake-in-2 false
+           snake-in-3 false
+           snake-in-4 false))))
+
+(deftest test-head-overlaps-body
+  (testing "Testing if the snake has collided with itself."
+    (let [snake-1 {:body '([10 5] [10 4] [10 3] [10 2] [10 1])}
+          snake-2 {:body '([10 4][11 4] [12 4] [12 5] [11 5] [10 5] [10 4] [10 3] [10 2] [10 1])}]
+      (are [snake target] (= target (head-overlaps-body? snake))
+           snake-1 false
+           snake-2 true))))
 
 (deftest test-change-direction
   (testing "Testing the snake direction change"
@@ -27,7 +55,7 @@
     (is (= DOWN (change-direction RIGHT RIGHT-TURN)))))
 
 (deftest test-move-no-grow
-  (testing "Testing the snake position change without grow"
+  (testing "Testing the snake position change without grow."
 
     (let [right-snake {:body '([8 5] [7 5] [6 5] [5 5] [4 5])}
           right-snake-forward {:body '([9 5] [8 5] [7 5] [6 5] [5 5])}
@@ -70,7 +98,7 @@
             down-snake-right down-snake LEFT))))
 
 (deftest test-move-grow
-  (testing "Testing the snake position change with grow"
+  (testing "Testing the snake position change with grow."
     (let [right-snake {:body '([8 5] [7 5] [6 5] [5 5] [4 5])}
           right-snake-forward {:body '([9 5] [8 5] [7 5] [6 5] [5 5] [4 5])}
           right-snake-up {:body '([8 4] [8 5] [7 5] [6 5] [5 5] [4 5])}
@@ -110,7 +138,7 @@
             down-snake-right down-snake LEFT))))
 
 (deftest test-food-ahead
-  (testing "Testing food detection (food ahead)"
+  (testing "Testing food detection (food ahead)."
     (let [snake {:body '([9 5])}
           apple-left {:location [2 5]}
           apple-right {:location [17 5]}
@@ -138,11 +166,77 @@
            DOWN apple-right false
            DOWN apple-up false))))
 
+(deftest test-danger-ahead
+  (testing "Testing danger detection (danger ahead)."
+    (let [snake-ovl {:body '([9 5] [10 5] [8 5] [9 4] [9 6])}
+          snake-ok {:body '([9 5] [11 5] [7 5] [9 3] [9 7])}
+          snake-top-corner {:body '([0 0])}
+          snake-bottom-corner {:body '([19 10])}]
+      (are [snake dir target] (= target (danger-ahead? snake dir))
+           snake-ovl LEFT true
+           snake-ovl RIGHT true
+           snake-ovl UP true
+           snake-ovl DOWN true
+           snake-ok LEFT false
+           snake-ok RIGHT false
+           snake-ok UP false
+           snake-ok DOWN false
+           snake-top-corner LEFT true
+           snake-top-corner RIGHT false
+           snake-top-corner UP true
+           snake-top-corner DOWN false
+           snake-bottom-corner LEFT false
+           snake-bottom-corner RIGHT true
+           snake-bottom-corner UP false
+           snake-bottom-corner DOWN true))))
 
+(deftest test-danger-right
+  (testing "Testing danger detection (danger right)."
+    (let [snake-ovl {:body '([9 5] [10 5] [8 5] [9 4] [9 6])}
+          snake-ok {:body '([9 5] [11 5] [7 5] [9 3] [9 7])}
+          snake-top-corner {:body '([0 0])}
+          snake-bottom-corner {:body '([19 10])}]
+      (are [snake dir target] (= target (danger-right? snake dir))
+           snake-ovl LEFT true
+           snake-ovl RIGHT true
+           snake-ovl UP true
+           snake-ovl DOWN true
+           snake-ok LEFT false
+           snake-ok RIGHT false
+           snake-ok UP false
+           snake-ok DOWN false
+           snake-top-corner LEFT true
+           snake-top-corner RIGHT false
+           snake-top-corner UP false
+           snake-top-corner DOWN true
+           snake-bottom-corner LEFT false
+           snake-bottom-corner RIGHT true
+           snake-bottom-corner UP true
+           snake-bottom-corner DOWN false))))
 
-
-
-
+(deftest test-danger-left
+  (testing "Testing danger detection (danger left)."
+    (let [snake-ovl {:body '([9 5] [10 5] [8 5] [9 4] [9 6])}
+          snake-ok {:body '([9 5] [11 5] [7 5] [9 3] [9 7])}
+          snake-top-corner {:body '([0 0])}
+          snake-bottom-corner {:body '([19 10])}]
+      (are [snake dir target] (= target (danger-left? snake dir))
+           snake-ovl LEFT true
+           snake-ovl RIGHT true
+           snake-ovl UP true
+           snake-ovl DOWN true
+           snake-ok LEFT false
+           snake-ok RIGHT false
+           snake-ok UP false
+           snake-ok DOWN false
+           snake-top-corner LEFT false
+           snake-top-corner RIGHT true
+           snake-top-corner UP true
+           snake-top-corner DOWN false
+           snake-bottom-corner LEFT true
+           snake-bottom-corner RIGHT false
+           snake-bottom-corner UP false
+           snake-bottom-corner DOWN true))))
 
 
 
