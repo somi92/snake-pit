95c783139543232f707b2021ae8db0b924925398
Milos Stojanovic <milos92s@yahoo.com>
2015-08-17 11:22:06 +0200

    Deleted run.

--- src/snake_pit/run.clj
+++
@@ -1,9 +0,0 @@
-(ns snake-pit.run
-  (:use snake-pit.core))
-
-(def gp-options
-  {:iterations 1 :migrations 1 :num-islands 4
-   :tournament-size 5 :population-size 500 :max-depth 3
-   :mutation-probability 0.3})
-
-(run-snakes-gp gp-options)
