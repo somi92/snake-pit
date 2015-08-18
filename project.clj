d6d7e2b1a3668bdf075e89097c9998b98ee74ccd
Milos Stojanovic <milos92s@yahoo.com>
2015-08-18 18:22:22 +0200

    Updating project file.

--- project.clj
+++ project.clj
@@ -1,11 +1,10 @@
-(defproject snake-pit "0.1.0-SNAPSHOT"
-  :description "Variant of The Snake game with autonomous snake behavior implemented using genetic programming."
+(defproject snake-pit "0.1.0"
+  :description "Evolving the snake game AI using genetic programming."
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [fungp "0.3.2"]
-                 [seesaw "1.4.5"]]
+                 [fungp "0.3.2"]]
   :main ^:skip-aot snake-pit.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
